8aa4a3e6b891405dcf3f35cefbe541eb0736da2f
Absolutiom <minh.sthk@gmail.com>
2024-04-09 23:51:09 +0200

    wip: add Vigenere decryption

--- src/Vigenere/VigenereCipher.java
+++ src/Vigenere/VigenereCipher.java
@@ -29,15 +29,12 @@ public class VigenereCipher {
         if (input.length() == key.length()) {
             result = key;
         }
-
-
         if (input.length() < key.length()) {
             for (int i = 0; i < keystream.length; i++) {
                 keystream[i] = keyToArr[i];
             }
             result = printCharArray(keystream);
         }
-
         if (input.length() > key.length()) {
 
             for (int i = 0; i < keystream.length; i++) {
@@ -46,15 +43,12 @@ public class VigenereCipher {
 
             result = printCharArray(keystream);
         }
-
-
         return result;
     }
 
     public String encode(String input, String key) {
         input = input.toUpperCase();
         key = key.toUpperCase();
-
         String encodedText = "";
         for (int i = 0; i < input.length(); i++) {
             // converting in range 0-25
@@ -65,14 +59,33 @@ public class VigenereCipher {
 
             encodedText += (char) (x);
         }
-
         return encodedText;
+    }
+
+    public String decode(String input, String key) {
+        input = input.toUpperCase();
+        key = key.toUpperCase();
+        String decodedText = "";
+        for (int i = 0; i < input.length(); i++) {
+            // converting in range 0-25
+            int x = (input.charAt(i) - key.charAt(i) + 26) % 26;
+
+            // convert into alphabets(ASCII)
+            x += 'A';
+            decodedText += (char) (x);
+        }
+        return decodedText;
     }
 
     public static void main(String[] args) {
         VigenereCipher cipher = new VigenereCipher("jackie", "abcde");
         String generatedKey = cipher.keyGeneration();
-        String encryption = cipher.encode(cipher.input, generatedKey);
+        String encryption = cipher.encode(cipher.getInput(), generatedKey);
         System.out.println(encryption);
+
+        VigenereCipher decryptionEx = new VigenereCipher("johan", "mess");
+        String generatedKey2 = decryptionEx.keyGeneration();
+        String decryption = decryptionEx.decode(decryptionEx.getInput(), generatedKey2);
+        System.out.println(decryption);
     }
 }
