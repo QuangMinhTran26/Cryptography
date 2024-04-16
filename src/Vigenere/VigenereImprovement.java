84284d5f66e100645245641433a9b42c76689f61
Absolutiom <minh.sthk@gmail.com>
2024-04-16 03:08:55 +0200

    wip: add an improvement for the Vigenere Cipher. First check if all the characters are alphabet, then encode

---
+++ src/Vigenere/VigenereImprovement.java
@@ -0,0 +1,46 @@
+package Vigenere;
+
+public class VigenereImprovement {
+    private String input;
+    private String key;
+
+    public VigenereImprovement(String input, String key) {
+        this.input = input;
+        this.key = key;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String alphabeticCheck(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isAlphabetic((input.charAt(i)))) {
+                input = input.replace(input.charAt(i), '°'); // there is no empty character, so  acts as a placeholder
+            }
+        }
+        input = input.replaceAll("°", "").toUpperCase(); // can not be put in the loop
+        return input;
+    }
+
+    public String encode(String input, String key) {
+        StringBuilder encodedText = new StringBuilder();
+        input = alphabeticCheck(input);
+        key = alphabeticCheck(key);
+
+        for (int i = 0; i < input.length(); i++) {
+            encodedText.append((char) ((input.charAt(i) + key.charAt(i % key.length())) % 26 + 65));
+        }
+        return encodedText.toString();
+    }
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
