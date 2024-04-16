89a733be53a30784b07db2752e202b7025338776
Absolutiom <minh.sthk@gmail.com>
2024-04-16 02:03:24 +0200

    wip: test isAlphabetic function -check if an character is an alphabet or not.

---
+++ src/TestTypeChar/AlphabeticCheck.java
@@ -0,0 +1,21 @@
+package TestTypeChar;
+
+public class AlphabeticCheck {
+    private static void printArray(char[] arr) {
+        for (char c : arr) {
+            System.out.print(c);
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "a dog";
+        printArray(s.toCharArray());
+        for (int i = 0; i < s.length(); i++) {
+            if (!Character.isAlphabetic(s.charAt(i))) {
+                s = s.replace(s.charAt(i), ('x'));
+            }
+        }
+        s = s.replaceAll("x", "").toUpperCase();
+        System.out.println(s);
+    }
+}
